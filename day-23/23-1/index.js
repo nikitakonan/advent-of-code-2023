d07d9df1086c41663f19e72959de3a1d41956fe2
nikitakonan <nikitakonan1991@gmail.com>
2023-12-25 13:16:06 +0400

    Day 23

--- day-23/23-1/index.js
+++ day-23/23-1/index.js
@@ -9,6 +9,81 @@ var SLOPE_UP = '^';
 var SLOPE_DOWN = 'v';
 var SLOPE_LEFT = '<';
 var SLOPE_RIGHT = '>';
+var TreeNode = /** @class */ (function () {
+    function TreeNode(position, distance) {
+        this.position = position;
+        this.distance = distance;
+    }
+    TreeNode.prototype.toString = function () {
+        return this.position.toString();
+    };
+    return TreeNode;
+}());
+function getPositions(_a) {
+    var x = _a[0], y = _a[1];
+    return [
+        [x, y - 1], // 0, up
+        [x + 1, y], // 1, right
+        [x, y + 1], // 2, down
+        [x - 1, y], // 3, left
+    ];
+}
+function getItemAt(map, _a) {
+    var x = _a[0], y = _a[1];
+    var row = map[y];
+    return row ? row[x] : undefined;
+}
+function getLongestPath(map, from, to) {
+    var longestPath = 0;
+    var root = new TreeNode(from, 0);
+    var visited = new Set();
+    var queue = [root];
+    visited.add(root.toString());
+    var _loop_1 = function () {
+        var currentRoot = queue.shift();
+        if (currentRoot.toString() === to.toString()) {
+            longestPath = currentRoot.distance;
+            console.log(longestPath);
+        }
+        var currentItem = getItemAt(map, currentRoot.position);
+        getPositions(currentRoot.position)
+            .filter(function (_coords, i) {
+            if (currentItem === SLOPE_DOWN) {
+                return i === 2;
+            }
+            else if (currentItem === SLOPE_UP) {
+                return i === 0;
+            }
+            else if (currentItem === SLOPE_LEFT) {
+                return i === 3;
+            }
+            else if (currentItem === SLOPE_RIGHT) {
+                return i === 1;
+            }
+            return true;
+        })
+            .filter(function (coords) {
+            var nextItem = getItemAt(map, coords);
+            return Boolean(nextItem) && nextItem !== FOREST;
+        })
+            .filter(function (pos) { return !visited.has(pos.toString()); })
+            .map(function (pos) { return new TreeNode(pos, currentRoot.distance + 1); })
+            .forEach(function (node) {
+            queue.push(node);
+            visited.add(node.toString());
+        });
+    };
+    while (queue.length > 0) {
+        _loop_1();
+    }
+    return longestPath;
+}
+function findStart(map) {
+    return [map[0].findIndex(function (el) { return el === PATH; }), 0];
+}
+function findEnd(map) {
+    return [map.at(-1).findIndex(function (el) { return el === PATH; }), map.length - 1];
+}
 (0, fs_1.readFile)((0, path_1.join)(__dirname, consts_1.TEST_INPUT), function (err, data) {
     if (err) {
         throw err;
@@ -17,5 +92,8 @@ var SLOPE_RIGHT = '>';
         .toString()
         .split('\n')
         .map(function (line) { return line.split(''); });
-    console.log(map.map(function (x) { return x.length; }));
+    var from = findStart(map);
+    var to = findEnd(map);
+    var shortestPath = getLongestPath(map, from, to);
+    console.log(shortestPath);
 });
--- day-23/23-1/index.ts
+++ day-23/23-1/index.ts
@@ -9,6 +9,91 @@ const SLOPE_DOWN = 'v';
 const SLOPE_LEFT = '<';
 const SLOPE_RIGHT = '>';
 
+type MapItem =
+  | typeof PATH
+  | typeof FOREST
+  | typeof SLOPE_UP
+  | typeof SLOPE_DOWN
+  | typeof SLOPE_LEFT
+  | typeof SLOPE_RIGHT;
+type Coords = [number, number];
+type Map<T> = T[][];
+
+class TreeNode {
+  position: Coords;
+  distance: number;
+  constructor(position: Coords, distance: number) {
+    this.position = position;
+    this.distance = distance;
+  }
+  toString() {
+    return this.position.toString();
+  }
+}
+
+function getPositions([x, y]: Coords): Coords[] {
+  return [
+    [x, y - 1], // 0, up
+    [x + 1, y], // 1, right
+    [x, y + 1], // 2, down
+    [x - 1, y], // 3, left
+  ];
+}
+
+function getItemAt<T>(map: Map<T>, [x, y]: Coords) {
+  const row = map[y];
+  return row ? row[x] : undefined;
+}
+
+function getLongestPath<T>(map: Map<T>, from: Coords, to: Coords) {
+  let longestPath = 0;
+  const root = new TreeNode(from, 0);
+  const visited = new Set<string>();
+  const queue = [root];
+  visited.add(root.toString());
+
+  while (queue.length > 0) {
+    const currentRoot = queue.shift();
+    if (currentRoot.toString() === to.toString()) {
+      longestPath = currentRoot.distance;
+      console.log(longestPath);
+    }
+    const currentItem = getItemAt(map, currentRoot.position);
+    getPositions(currentRoot.position)
+      .filter((_coords, i) => {
+        if (currentItem === SLOPE_DOWN) {
+          return i === 2;
+        } else if (currentItem === SLOPE_UP) {
+          return i === 0;
+        } else if (currentItem === SLOPE_LEFT) {
+          return i === 3;
+        } else if (currentItem === SLOPE_RIGHT) {
+          return i === 1;
+        }
+        return true;
+      })
+      .filter((coords) => {
+        const nextItem = getItemAt(map, coords);
+        return Boolean(nextItem) && nextItem !== FOREST;
+      })
+      .filter((pos) => !visited.has(pos.toString()))
+      .map((pos) => new TreeNode(pos, currentRoot.distance + 1))
+      .forEach((node) => {
+        queue.push(node);
+        visited.add(node.toString());
+      });
+  }
+  return longestPath;
+}
+
+function findStart<T>(map: Map<T>): Coords {
+  return [map[0].findIndex((el) => el === PATH), 0];
+}
+
+function findEnd<T>(map: Map<T>): Coords {
+  return [map.at(-1).findIndex((el) => el === PATH), map.length - 1];
+}
+
 readFile(join(__dirname, TEST_INPUT), (err, data) => {
   if (err) {
     throw err;
@@ -17,6 +102,10 @@ readFile(join(__dirname, TEST_INPUT), (err, data) => {
   const map = data
     .toString()
     .split('\n')
-    .map((line) => line.split(''));
-  console.log(map.map((x) => x.length));
+    .map((line) => line.split('') as MapItem[]);
+
+  const from = findStart(map);
+  const to = findEnd(map);
+  const shortestPath = getLongestPath(map, from, to);
+  console.log(shortestPath);
 });
